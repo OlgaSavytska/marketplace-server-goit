db6c587ead476ef33e173b0b5e4bfa667d99a794
OlgaSavytska <o.savytska77@gmail.com>
2019-11-18 08:54:08 +0200

    1

---
+++ src/routes/products/orders.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const fs = require("fs");
+
+
+const getOrder = (request, response) => {
+    const productsFile = path.join(__dirname, '../../../', '/src/db/products', '/all-products.json');
+    const productsFileData = fs.readFileSync(productsFile);
+    const products = JSON.parse(productsFileData);
+
+    const usersFile = path.join(__dirname, '../../../',  '/src/db/users', '/all-users.json');
+    const usersFileData = fs.readFileSync(usersFile);
+    const users = JSON.parse(usersFileData);
+    let responseObj ={};
+
+    console.log(arrNormal(request.body.products).length);
+
+  if (getElementsById(request.body.products).length !== arrNormal(request.body.products).length) {
+      responseObj = {
+          'status': 'failed',
+          'order': null
+      }
+  } else {
+       responseObj = {
+          "status": "success",
+          "order": {
+              "id": "id",
+              "user": request.body.user,
+              "products": request.body.products,
+              "deliveryType": request.body.deliveryType,
+              "deliveryAdress": request.body.deliveryAdress
+          }};
+  }
+
+
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.write(JSON.stringify(responseObj));
+        response.end();
+
+
+
+};
+
+function arrNormal(arr){
+    let idArr = arr.replace('[','');
+    idArr = idArr.replace(']','');
+    idArr = idArr.replace(', ',',');
+    idArr = idArr.replace(', ',',');
+    const idArray = idArr.split(',');
+    return idArray;
+}
+
+function getElementsById(idArr) {
+    const productsFile = path.join(__dirname, '../../../',  '/src/db/products', '/all-products.json');
+    const products = fs.readFileSync(productsFile);
+    const allProductsJS = JSON.parse(products);
+    let objectItems = [];
+
+    let idArray = arrNormal(idArr);
+    for (let idNumber of idArray){
+
+        for (let productItem of allProductsJS){
+            idNumber = Number(idNumber);
+            if(idNumber === productItem.id){
+                objectItems.push(productItem);
+            }
+        }
+    }
+
+    return objectItems;
+}
+
+module.exports = getOrder;
