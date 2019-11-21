7024bafda4835c6df6f5c98157aedaf85409b346
OlgaSavytska <o.savytska77@gmail.com>
2019-11-21 08:30:56 +0200

    hw

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = require('./modules/app');
+const morgan = require('morgan');
+const router = require('./routes/router');
+
+const errorHandler = (req, res, next)  => {
+    res.status(500).send('No such page');
+    next();
+};
+
+const staticPath = path.join(__dirname, '..', 'assets');
+
+const startServer = port => {
+    app
+        .use(bodyParser.urlencoded({ extended: false }))
+        .use(bodyParser.json())
+        .use(morgan('dev'))
+        .use(express.static(staticPath))
+        .use('/', router)
+        .use(errorHandler);
+
+    app.listen(port);
+
+    console.log('Server was started at http://localhost:' + port);
+};
+
+module.exports = startServer;
