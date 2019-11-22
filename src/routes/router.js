69fb1de3187149362ebd1ea31202ec6b8fde6137
OlgaSavytska <o.savytska77@gmail.com>
2019-11-22 12:03:59 +0200

    hw2.6

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,12 +2,6 @@ const mainRoute = require('./main/main');
 const signUpRoute = require('./users/sign-up-route');
 const productRequest = require('./products/products');
 
-const router = {
-  '/signup': signUpRoute,
-  '/products/': productRequest,
-  '/products not/': products,
-  default: mainRoute
-};
 
 
 module.exports = router;
--- src/routes/users/sign-up-route.js
+++ src/routes/users/sign-up-route.js
@@ -4,23 +4,23 @@ const path = require("path");
 const signUpRoute = (request, response) => {
     if (request.method === 'POST') {
 
-        request.on('data', function (data) {
-            request.on('end', function () {
-                let receivedData = JSON.parse(data);
-                let username = receivedData.name;
+        request.on('end', function (data) {
 
-                fs.writeFile(
-                    path.join(__dirname, "../../db/users/", `${username}.json`),
-                    data,
-                    err => {
-                        if (err) throw err;
-                    }
-                );
-                response.writeHead(200, { "Content-Type": "application/json" });
-                response.write(JSON.stringify({ status: "success", user: username }));
-                response.end();
-            });
+            let receivedData = JSON.parse(data);
+            let username = receivedData.name;
+
+            fs.writeFile(
+                path.join(__dirname, "../../db/users/", `${username}.json`),
+                data,
+                err => {
+                    if (err) throw err;
+                }
+            );
+            response.writeHead(200, { "Content-Type": "application/json" });
+            response.write(JSON.stringify({ status: "success", user: username }));
+            response.end();
         });
+
     } else {
         response.writeHead(400, { "Content-Type": "text/html" });
         response.write(
